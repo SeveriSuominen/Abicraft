b33cfc9a3edb5b2eae9f055a5b5a665f4ad7072a
severi.suominen <seve.suomi@gmail.com>
2020-01-03 18:56:47 +0200

    Abicraft inspector resizer fixes

--- Assets/ABICraft/Editor/Test/AbicraftInspector.cs
+++ Assets/ABICraft/Editor/Test/AbicraftInspector.cs
@@ -11,12 +11,22 @@ using AbicraftMonos;
 public class AbicraftInspector : EditorWindow
 {
     static string filter = "", activeTab;
-    private static Vector2 scrollPos = Vector2.zero;
+    static readonly List<Object> selectedBuffer = new List<Object>();
+   
     static readonly List<AbicraftObject> scannedSceneAbjs = new List<AbicraftObject>();
-    float currentScrollViewWidth;
+
+
+    //SCENE OBJECT VIEW
+    float sceneObjectViewSplitWidth, sceneObjectViewSplitHeight;
+    Rect sceneObjectViewSplitWidthCursorRect, sceneObjectViewSplitHeightCursorRect;
+    static Vector2 sceneObjectScrollView = Vector2.zero, assetObjectScrollView = Vector2.zero;
+
+    bool resizeHorizontal = false, resizeVertical = false;
+    //-----------------
+
     Vector2 scrollPosition = Vector2.zero;
-    bool resize = false;
-    Rect cursorChangeRect;
+
+
 
     static int selectedTab = 0;
 
@@ -62,9 +72,9 @@ public class AbicraftInspector : EditorWindow
         GUILayout.EndHorizontal();
         //Restore color
         GUI.backgroundColor = storeColor;
-        //Draw a line over the bottom part of the buttons (ugly haxx)
+      
         var texture = new Texture2D(1, 1);
-        texture.SetPixel(0, 0, highlightCol);
+        texture.SetPixel(0, 0, bgCol);
         texture.Apply();
         GUI.DrawTexture(new Rect(0, buttonStyle.lineHeight + buttonStyle.border.top + buttonStyle.margin.top + StartSpace, Screen.width, 4), texture);
 
@@ -73,21 +83,21 @@ public class AbicraftInspector : EditorWindow
 
     void OnEnable()
     {
-        //this.position = new Rect(200, 200, 400, 300);
-        currentScrollViewWidth = this.position.height / 2;
-        cursorChangeRect = new Rect(currentScrollViewWidth, 0, 5f, this.position.width);
+        //SCENE OBJECT SPLIT WIDTH
+        sceneObjectViewSplitWidth = this.position.width / 2;
+        sceneObjectViewSplitWidthCursorRect = new Rect(sceneObjectViewSplitWidth, 0, 3f, this.position.width);
+
+        sceneObjectViewSplitHeight = this.position.height / 2;
+        sceneObjectViewSplitHeightCursorRect= new Rect(0, sceneObjectViewSplitHeight, sceneObjectViewSplitWidth -22, 3f); 
     }
 
 
-    static Color coldef;
-
-    void OnGUI()
+    void SceneObjectsView()
     {
-        Tabs(ref selectedTab, "Setup", "Abilities", "States", "Attributes", "Pooling");
-        coldef = GUI.backgroundColor;
+        Color coldef = GUI.backgroundColor;
 
         GUI.backgroundColor = new Color32(184, 182, 182, 255);
-        GUILayout.BeginArea(new Rect(0, 30, currentScrollViewWidth + 20, this.position.height - 30));
+        GUILayout.BeginArea(new Rect(0, 30, sceneObjectViewSplitWidth + 20, position.height));
         GUILayout.BeginVertical();
 
         EditorGUILayout.BeginHorizontal();
@@ -96,7 +106,7 @@ public class AbicraftInspector : EditorWindow
         GUIStyle noCancelStyle = GUI.skin.FindStyle("ToolbarSeachCancelButtonEmpty");
 
         GUILayout.Space(10);
-        filter = EditorGUILayout.TextField(filter, searchStyle, GUILayout.Width(currentScrollViewWidth - 30));
+        filter = EditorGUILayout.TextField(filter, searchStyle, GUILayout.Width(sceneObjectViewSplitWidth - 30));
         if (!string.IsNullOrEmpty(filter))
         {
             if (GUILayout.Button("", cancelStyle))
@@ -113,19 +123,35 @@ public class AbicraftInspector : EditorWindow
         GUILayout.Space(10);
         EditorGUILayout.EndHorizontal();
 
-        scrollPos = GUILayout.BeginScrollView(scrollPos, GUILayout.Width(currentScrollViewWidth));
+        sceneObjectScrollView = GUILayout.BeginScrollView(sceneObjectScrollView, GUILayout.Width(sceneObjectViewSplitWidth), GUILayout.Height(sceneObjectViewSplitHeight));
 
-        /*foreach(var abj in scannedSceneAbjs)
-         {
-             GUILayout.Label(abj.name);
-         }*/
         CreateTree();
+
         GUILayout.EndScrollView();
+
+        //GUILayout.EndVertical();
+        //GUILayout.EndArea();
         
-        ResizeScrollView();
+        ResizeScrollView(ref sceneObjectViewSplitWidth, ref sceneObjectViewSplitWidthCursorRect, ref resizeHorizontal, ResizeDir.Horizontal);
+
+        //GUILayout.BeginArea(new Rect(0, sceneObjectViewSplitHeight, sceneObjectViewSplitWidth + 20, position.height));
+        //GUILayout.BeginVertical();
+
+        assetObjectScrollView = GUILayout.BeginScrollView(assetObjectScrollView, GUILayout.Width(sceneObjectViewSplitWidth), GUILayout.Height(position.height - sceneObjectViewSplitHeight));
+
+        CreateTree();
+
+        GUILayout.EndScrollView();
+
+        ResizeScrollView(ref sceneObjectViewSplitHeight, ref sceneObjectViewSplitHeightCursorRect, ref resizeVertical, ResizeDir.Vertical);
+
+        GUILayout.EndVertical();
+        GUILayout.EndArea();
+
         GUILayout.FlexibleSpace();
         GUI.backgroundColor = coldef;
-        if (GUILayout.Button("Scan", GUILayout.Width(currentScrollViewWidth -5), GUILayout.Height(60)))
+    
+        /*if (GUILayout.Button("Scan", GUILayout.Width(currentScrollViewWidth - 5), GUILayout.Height(60)))
         {
             CreateTree();
             scannedSceneAbjs.Clear();
@@ -143,15 +169,26 @@ public class AbicraftInspector : EditorWindow
                     scannedSceneAbjs.Add(objs[i] as AbicraftObject);
                 }
             }
-        }
-
-        GUILayout.EndVertical();
-        GUILayout.EndArea();
+        }*/
 
         Repaint();
 
         GUILayout.FlexibleSpace();
     }
+
+    void OnGUI()
+    {
+        selectedTab = Tabs(ref selectedTab, "Setup", "Scene Objects", "Abilities", "States", "Attributes", "Pooling");
+
+        switch (selectedTab)
+        {
+            case 1:
+                SceneObjectsView();
+                    break;
+        }
+    }
+
+    static bool keyDown = false;
 
     void DrawName(Transform transform)
     {
@@ -172,73 +209,122 @@ public class AbicraftInspector : EditorWindow
         Rect rect = EditorGUILayout.GetControlRect();
         rect = EditorGUI.IndentedRect(rect);
 
-        Rect marker = new Rect(10, rect.y + 8, rect.x - 20, 1);
+        float rectX = rect.x; //Using as a padding
+        rect.width = sceneObjectViewSplitWidth;
+        rect.x = 0;
 
-      
+        style.padding.left = Mathf.FloorToInt(rectX);
+
+        Rect marker = new Rect(10, rect.y + 8, rectX - 20, 1);
 
         GUIContent content;
 
         if (transform.GetComponent<AbicraftObject>() || transform.GetComponent<Abicraft>())
         {
-            GUI.DrawTexture(marker, EditorGUIUtility.whiteTexture);
+            Color bgcolordef = GUI.backgroundColor;
 
             style.hover.textColor = Color.blue;
-
             style.normal.textColor = new Color32(219, 29, 29, 255);
 
+            if (selectedBuffer.Contains(transform.gameObject))
+            {
+                GUI.backgroundColor = new Color32(217, 217, 217, 255);
+            }
+
             content = new GUIContent(transform.name);
+
+            if (Event.current.type == EventType.KeyDown && Event.current.keyCode == (KeyCode.LeftControl))
+            {
+                keyDown = true;
+            }
+
+            if (Event.current.type == EventType.KeyUp && Event.current.keyCode == (KeyCode.LeftControl))
+            {
+                keyDown = false;
+            }
+
             if (GUI.Button(rect, content, style))
             {
-
+                if (keyDown)
+                {
+                    selectedBuffer.Add(transform.gameObject);
+                }
+                else
+                {
+                    selectedBuffer.Clear();
+                    selectedBuffer.Add(transform.gameObject);
+                }
             }
 
             Rect iconRect = new Rect(marker);
 
             iconRect.width = 16;
             iconRect.height = 16;
-            iconRect.x = currentScrollViewWidth - 35;
+            iconRect.x = sceneObjectViewSplitWidth - 35;
             iconRect.y = iconRect.y - 6;
 
+            GUI.DrawTexture(marker, EditorGUIUtility.whiteTexture);
             GUI.DrawTexture(iconRect, AbicraftNodeEditor.NodeEditorResources.abicraftIcon);
+
+            GUI.backgroundColor = bgcolordef;
         }
         else
         {
-           // float useIntend = (EditorGUI.indentLevel) * 20;
-           // marker.width = useIntend -20;
+            style.normal.textColor = new Color32(100,100,100, 255);
+            content = new GUIContent(transform.name);
+
+            GUI.Label(rect, content, style);
 
             Color defcol = GUI.color;
             GUI.color = new Color32(156, 154, 154, 255);
             GUI.DrawTexture(marker, EditorGUIUtility.whiteTexture);
             GUI.color = defcol;
-
-            style.normal.textColor = new Color32(100,100,100, 255);
-            content = new GUIContent(transform.name);
-           //rect.x = useIntend;
-
-            GUI.Label(rect, content, style);
         }
     }
 
-    private void ResizeScrollView()
+    public enum ResizeDir
     {
-        GUI.DrawTexture(cursorChangeRect, EditorGUIUtility.whiteTexture);
-        EditorGUIUtility.AddCursorRect(cursorChangeRect, MouseCursor.ResizeHorizontal);
+        Horizontal,
+        Vertical
+    }
 
-        if (Event.current.type == EventType.MouseDown && cursorChangeRect.Contains(Event.current.mousePosition))
+    private void ResizeScrollView(ref float axel, ref Rect cursorRect, ref bool resize, ResizeDir dir)
+    {
+        GUI.DrawTexture(cursorRect, EditorGUIUtility.whiteTexture);
+        EditorGUIUtility.AddCursorRect(cursorRect, dir == ResizeDir.Horizontal ? MouseCursor.ResizeHorizontal : MouseCursor.ResizeVertical);
+
+        if (Event.current.type == EventType.MouseDown && cursorRect.Contains(Event.current.mousePosition))
         {
             resize = true;
         }
         if (resize)
         {
-            currentScrollViewWidth = Event.current.mousePosition.x;
+            if(dir == ResizeDir.Horizontal)
+            {
+                axel = Event.current.mousePosition.x;
 
-            currentScrollViewWidth = currentScrollViewWidth > position.width - 50 ? position.width - 50 : currentScrollViewWidth;
-            currentScrollViewWidth = currentScrollViewWidth < 50 ? 50 : currentScrollViewWidth;
+                axel = axel > position.width - 50 ? position.width - 50 : axel;
+                axel = axel < 50 ? 50 : axel;
 
-            cursorChangeRect.Set(currentScrollViewWidth, cursorChangeRect.y, cursorChangeRect.width, position.height);
+                cursorRect.Set(axel, cursorRect.y, cursorRect.width, position.height);
+            }
+
+            if (dir == ResizeDir.Vertical)
+            {
+                Debug.Log(Event.current.mousePosition.y + " ::: " + Event.current.type);
+
+                axel = Event.current.mousePosition.y;
+
+              //  axel = axel > position.height - 50 ? position.height - 50 : axel;
+               // axel = axel < 50 ? 50 : axel;
+
+                cursorRect.Set(cursorRect.x, axel +15, sceneObjectViewSplitWidth - 32, cursorRect.height);
+            }
         }
         if (Event.current.type == EventType.MouseUp)
+        {
             resize = false;
+        }   
     }
 
     void CreateTree()
