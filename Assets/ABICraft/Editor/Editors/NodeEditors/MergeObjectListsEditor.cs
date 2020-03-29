d9f5e6d0bc341ce00c0708c792cc745614ea46f9
severi.suominen <seve.suomi@gmail.com>
2020-03-29 20:10:14 +0300

    - Merge Object Lists node

---
+++ Assets/ABICraft/Editor/Editors/NodeEditors/MergeObjectListsEditor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+using AbicraftNodeEditor;
+using AbicraftNodes.Action;
+using UnityEditor;
+using AbicraftCore;
+using AbicraftMonos;
+using System;
+using AbicraftNodes.Meta;
+
+using AbicraftNodeEditor;
+using AbicraftNodes.Meta;
+using AbicraftNodes.Math;
+using AbicraftNodes.Object;
+
+namespace AbicraftNodes.Editors
+{
+    [CustomNodeEditor(typeof(MergeObjectListsNode))]
+    public class MergeObjectListsEditor : NodeEditor
+    {
+        MergeObjectListsNode node;
+
+        int lastSelectedIndex;
+   
+
+        public override void OnHeaderGUI(GUIStyle style)
+        {
+            // Initialization
+            if (node == null)
+            {
+                node = target as MergeObjectListsNode;
+            }
+
+            base.OnHeaderGUI(style);
+
+            Rect dotRect = GUILayoutUtility.GetLastRect();
+            dotRect.size = new Vector2(16, 16);
+            dotRect.y += 6;
+
+            /*
+            GUI.color = Color.red;
+            GUI.DrawTexture(dotRect, NodeEditorResources.dot);
+            GUI.color = Color.white;*/
+        }
+
+        public override void OnBodyGUI()
+        {
+            GuiSpace(10);
+            node = target as MergeObjectListsNode;
+
+            GUILayout.BeginHorizontal();
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("ObjectList"),    new GUIContent("Lists"));
+            NodeEditorGUILayout.PropertyField(serializedObject.FindProperty("ObjectListOut"), new GUIContent("Merged"));
+            GUILayout.EndHorizontal();
+        }
+    }
+}
---
+++ Assets/ABICraft/Scripts/Nodes/MergeObjectListsNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using AbicraftNodeEditor;
+using AbicraftCore;
+using AbicraftMonos;
+using AbicraftNodes.Meta;
+
+namespace AbicraftNodes.Object
+{
+	public class MergeObjectListsNode : AbicraftValueNode {
+
+		/* Example of node input field */
+		[Input(backingValue = ShowBackingValue.Never, connectionType = ConnectionType.Multiple, typeConstraint = TypeConstraint.Strict)]
+		public List<AbicraftObject> ObjectList; 
+
+		/* Example node output field */
+		[Output]
+		public List<AbicraftObject> ObjectListOut; 
+
+		// Return value when another node is requesting output field value
+		public override object GetValue(AbicraftNodeExecution e, NodePort port) {
+			//Example returning of data
+			switch(port.fieldName)
+			{
+				case "ObjectListOut":
+                    var values = GetInputValues<List<AbicraftObject>>(e, "ObjectList");
+                    var merged = new List<AbicraftObject>();
+
+                    for (int i = 0; i < values.Length; i++)
+                    {
+                        for (int j = 0; j < values[i].Count; j++)
+                        {
+                            merged.Add(values[i][j]);
+                        }
+                    }
+					return merged;
+				default:
+					return default;
+			}
+		}
+	}
+}
